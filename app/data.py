b942958b827577322a69ba8f9316784acb347ade
atalvarez1 <alvarez.adamt@gmail.com>
2024-02-05 21:23:57 -0500

    Created Database class as part of Sprint 1 Ticket

--- app/data.py
+++ app/data.py
@@ -6,20 +6,54 @@ from MonsterLab import Monster
 from pandas import DataFrame
 from pymongo import MongoClient
 
-#test
+
 class Database:
 
+    def __init__(self, collection_name='monsters'):
+        """
+        Initializing statement, initiates connection with MongoDB and 
+        sets collection name
+        """
+        load_dotenv()
+        db_url = getenv('DB_URL')
+        db_name = getenv('DB_NAME')
+
+        self.database = MongoClient(db_url, tlsCAFile=where())[db_name]
+        self.collection = self.database[collection_name]
+
     def seed(self, amount):
-        pass
+        """
+        Inserts the specified number of random monsters into the collection
+        """
+        monsters = [Monster().to_dict() for _ in range(amount)]
+        return self.collection.insert_many(monsters)
 
     def reset(self):
-        pass
+        """
+        Deletes all documents from the collection
+        """
+        return self.collection.delete_many({})
 
     def count(self) -> int:
-        pass
+        """
+        Returns the number of documents in the collection
+        """
+        return self.collection.count_documents({})
 
     def dataframe(self) -> DataFrame:
-        pass
+        """
+        Returns a DataFrame containing all documents in the collection
+        """
+        documents = self.collection.find(())
+        df = DataFrame(documents)
+        new_df = df.drop('_id', axis=1)
+        return new_df
 
     def html_table(self) -> str:
-        pass
+        """
+        Returns an HTML table representation of the DataFrame, 
+        or None if the collection is empty
+        """
+        if self.count() == 0:
+            return None
+        return self.dataframe().to_html()
